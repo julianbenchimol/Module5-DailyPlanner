eac613e7a0ec5bde96530066fd503ff5e66e1ad0
Julian Benchimol <jbenchimol1999@gmail.com>
2022-10-14 09:32:46 -0400

    added in working time elemeent that updates in real time

---
+++ assets/JS/MainScript.js
@@ -0,0 +1,13 @@
+var timeViewElement;
+var now;
+
+//Gets the time and updates the HTML element in real time
+var UpdateTime = function(){
+    now = moment();
+    timeViewElement.text(now.format('dddd, MMMM do YYYY, hh:mm:ss a'));
+}
+$(document).ready(function(){
+    timeViewElement = $('#time-view')
+    UpdateTime();
+    setInterval(UpdateTime, 1000);
+});
